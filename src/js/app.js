6f8887785667af98ab502bffa67f7a6daac6895b
majsy <madeleineauvinen@gmail.com>
2018-08-23 13:09:55 -0400

    add project cover images add project link hover state

--- src/js/app.js
+++ src/js/app.js
@@ -6,7 +6,36 @@ class App {
 
     new Header();
     new Footer();
+    // this.loadData();
+
+    this.dataIsLoaded = false;
   }
+
+  // loadData() {
+  //   if (this.dataIsLoaded) return;
+
+  //   this.loadAppData()
+  //     .then(() => {
+  //       if (this.dataIsLoaded) {
+  //         this.dataIsLoaded = true;
+  //       }
+  //     })
+  // }
+
+  // loadAppData = () => {
+  //   return fetch(END_POINT.APP)
+  //     .then(response => {
+  //       if (response) {
+  //         return response.json()
+  //       }
+  //     })
+  //     .then((appData) => {
+  //       this.setState({appData})
+  //     })
+  //     .catch((e) => {
+  //       return e
+  //     })
+  // }
 }
 
 new App();
